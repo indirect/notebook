3c154cc5cfb4662b49ff63646e7b0fb320d38f4d
Andre Arko <andre@arko.net>
2018-07-17 11:26:07 -0700

    ruby notebook with gems

---
+++ gems.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# A sample gems.rb
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "iruby", "~> 0.3"
+
+gem "iruby-dependencies", "~> 2.1"
+
+gem "http", "~> 3.3"
+
+gem "cztop", "~> 0.13.1"
+
+gem "pry", "~> 0.11.3"
+
+gem "pry-doc", "~> 0.13.4"
+
+gem "awesome_print", "~> 1.8"
+
+gem "gnuplot", "~> 2.6"
+
+gem "rubyvis", "~> 0.6.1"
+
+gem "nyaplot", "~> 0.1.6"
